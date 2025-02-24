7db93ba85cc8742b79ae4ebff607a697fac6739e
Maksim <NeoRiZe456D@gmail.com>
2025-02-24 10:30:53 +0300

    Bug fixes

--- Source/Binary_Operations.py
+++ Source/Binary_Operations.py
@@ -1,3 +1,7 @@
+from math import remainder
+import struct
+
+
 def decimal_to_binary_direct_code(decimal_num: int) -> str:
 
     if decimal_num == 0:
@@ -72,6 +76,14 @@ def binary_to_decimal_direct_code(binary_num: str) -> int:
 
     return decimal_num
 
+def binary_to_decimal_fract(binary_num: str) -> int:
+    decimal_fract = 0
+
+    for i, digit in enumerate(binary_num[0:]):
+        decimal_fract += float(float(digit) * (2 ** (-1 * (i + 1 ))))
+
+    return decimal_fract
+
 def binary_to_decimal_additional_code(additional_binary_num: str) -> int:
 
     sign = additional_binary_num[0]
@@ -109,13 +121,13 @@ def binary_multiply(first_binary: str, second_binary: str) -> str:
     for i in range(len(first_binary)):
         if first_binary[i] == '1':
             shifted_second_binary = second_binary + '0' * i
-            result = binary_add(result, shifted_second_binary)
+            result = binary_add_shifted(result, shifted_second_binary)
 
     result_value = result[-7:].zfill(7)
 
     return str(sign_value) + result_value
 
-def binary_add(first_binary: str, second_binary: str) -> str:
+def binary_add_shifted(first_binary: str, second_binary: str) -> str:
     max_len = max(len(first_binary), len(second_binary))
     first_binary = first_binary.zfill(max_len)
     second_binary = second_binary.zfill(max_len)
@@ -133,158 +145,138 @@ def binary_add(first_binary: str, second_binary: str) -> str:
 
     return ''.join(result_binary[::-1])
 
+def binary_remainder_divide(remainder: str, divisor:str) -> str:
+    divisor_length = len(divisor)
+    remainder_dividend = remainder.lstrip('0') + '000000'
+    remainder_quotient = ''
+    remainder = remainder.lstrip('0')
+    if remainder_dividend == '0000000':
+        return '00000'
+    else:
+        for bit in remainder_dividend[len(remainder) + 1:]:
+            remainder += bit
+            if len(remainder) >= divisor_length and int(remainder) >= int(divisor):
+                remainder = binary_subtract(remainder, divisor)
+                remainder_quotient += "1"
+            else:
+                remainder_quotient += "0"
+    return remainder_quotient
 
-def binary_division(dividend: str, divisor: str) -> str:
-    if divisor == "00000000":
-        return "Zero Division Error"
+def binary_integer_divide(dividend: str, divisor: str) -> tuple:
 
-    sign_value = '1' if dividend[0] != divisor[0] else '0'
+    quotient = remainder = ""
+    dividend = dividend.lstrip('0')
+    divisor_length = len(divisor)
+
+    for bit in dividend:
+        remainder += bit
+        if len(remainder) >= divisor_length and remainder >= divisor:
+            remainder = binary_subtract(remainder, divisor)
+            remainder = remainder.lstrip('0')
+            quotient += "1"
+        else:
+            remainder = remainder.lstrip('0')
+            quotient += "0"
+    return quotient, remainder
+
+def binary_divide(dividend: str, divisor: str) -> str:
+
+    sign_value = '0' if divisor[0] == dividend[0] else '1'
 
     dividend = dividend[1:]
     divisor = divisor[1:]
 
-    dividend = dividend.lstrip('0')
-    divisor = divisor.lstrip('0')
+    if divisor == "0":
+        raise ValueError("Zero Division Error")
 
-    if not dividend:
+    dividend = dividend.lstrip('0')
+    divisor_length = len(divisor)
+
+    if divisor_length == 0 or dividend == "":
         return "0"
 
-    if len(dividend) < len(divisor) or (len(dividend) == len(divisor) and dividend < divisor):
-        return "0" + '.' + dividend
+    integer_quotient, remainder = binary_integer_divide(dividend, divisor)
+    remainder_quotient = binary_remainder_divide(remainder, divisor)
 
-    quotient = ''
-    remainder = ''
+    integer_quotient = integer_quotient.lstrip('0') or "0"
+    return sign_value + integer_quotient + '.' + remainder_quotient
 
-    for bit in dividend:
-        remainder += bit
+def binary_to_decimal_division(binary_value: str) -> float:
+    sign_value = 1 if binary_value[0] == '0' else -1
+    binary_value = binary_value[1:]
+    integer_binary, fract_binary = binary_value.split('.')
+    integer_decimal = binary_to_decimal_direct_code(integer_binary.zfill(8))
+    fract_decimal = binary_to_decimal_fract(fract_binary)
+    return sign_value * (integer_decimal + fract_decimal)
 
-        if remainder >= divisor:
-            remainder = binary_subtract(remainder, divisor)
-            quotient += '1'
-        else:
-            quotient += '0'
-
-    dividend = (remainder + "00000")[:6]
-    remainder = ''
-    remainder_quotient = ''
-    if dividend == '000000':
-        return sign_value + quotient.zfill(7) + '.' + '00000'
-    else:
-        for bit in dividend:
-            remainder += bit
-            if remainder >= divisor:
-                remainder = binary_subtract(remainder, divisor)
-                remainder_quotient += '1'
-            else:
-                remainder_quotient += '0'
-
-    remainder_quotient = quotient.lstrip('0') or '0'
-
-    return sign_value + quotient.zfill(7) + '.' + remainder_quotient
-
-
-def binary_subtract(bin1: str, bin2: str) -> str:
-    max_len = max(len(bin1), len(bin2))
-
-    bin1 = bin1.zfill(max_len)
-    bin2 = bin2.zfill(max_len)
-
+def binary_subtract(minuend: str, subtrahend: str) -> str:
+    max_length = max(len(minuend), len(subtrahend))
+    minuend = minuend.zfill(max_length)
+    subtrahend = subtrahend.zfill(max_length)
     result = []
     borrow = 0
-
-    for i in range(max_len - 1, -1, -1):
-        bit1 = int(bin1[i])
-        bit2 = int(bin2[i])
-
-        if borrow:
-            if bit1 == 0:
-                bit1 = 1
-                borrow = 1
-            else:
-                bit1 = 0
-                borrow = 0
-
-        if bit1 < bit2:
-            bit1 += 2
+    for m, s in zip(reversed(minuend), reversed(subtrahend)):
+        m_bit = int(m)
+        s_bit = int(s)
+        m_bit -= borrow
+        if m_bit < s_bit:
+            m_bit += 2
             borrow = 1
-
-        result_bit = bit1 - bit2
-        result.append(str(result_bit))
-
-    return ''.join(result[::-1]).lstrip('0') or '0'
-
-import struct
-
-def float_to_binary(num:float):
-    packed = struct.pack('>f', num)
-    return ''.join(f'{byte:08b}' for byte in packed)
-
-def binary_to_float(b):
-    return struct.unpack('>f', int(b, 2).to_bytes(4, byteorder='big'))[0]
-
-def sum_floats(a:float, b:float) -> float:
-    a_bin = float_to_binary(a)
-    b_bin = float_to_binary(b)
-
-    sign_a, exponent_a, mantissa_a = a_bin[0], int(a_bin[1:9], 2), a_bin[9:]
-    sign_b, exponent_b, mantissa_b = b_bin[0], int(b_bin[1:9], 2), b_bin[9:]
-
-    exponent_a -= 127
-    exponent_b -= 127
-
-    if exponent_a > exponent_b:
-        exponent_b = exponent_a
-        mantissa_b = mantissa_b.lstrip('0')
-        mantissa_b = mantissa_b + '0' * (23 - len(mantissa_b))
-    elif exponent_a < exponent_b:
-        exponent_a = exponent_b
-        mantissa_a = mantissa_a.lstrip('0')
-        mantissa_a = mantissa_a + '0' * (23 - len(mantissa_a))
-
-    if sign_a == sign_b:
-        mantissa_sum = bin(int(mantissa_a, 2) + int(mantissa_b, 2))[2:]
-        sign_result = sign_a
-    else:
-        mantissa_a_int = int(mantissa_a, 2)
-        mantissa_b_int = int(mantissa_b, 2)
-        if mantissa_a_int >= mantissa_b_int:
-            mantissa_sum = bin(mantissa_a_int - mantissa_b_int)[2:]
-            sign_result = sign_a
         else:
-            mantissa_sum = bin(mantissa_b_int - mantissa_a_int)[2:]
-            sign_result = sign_b
+            borrow = 0
+        result_bit = m_bit - s_bit
+        result.append(str(result_bit))
+    while len(result) > 1 and result[-1] == '0':
+        result.pop()
+    return ''.join(reversed(result))
 
-    if len(mantissa_sum) > 23:
-        mantissa_sum = mantissa_sum[:-1]
-        exponent_a += 1
+def float_to_binary(value):
+    int_representation = struct.unpack('!I', struct.pack('!f', value))[0]
 
-    new_mantissa = mantissa_sum.zfill(23)
-    new_exponent = format(exponent_a + 127, '08b')
-
-    result_bin = sign_result + new_exponent + new_mantissa
-
-    return binary_to_float(result_bin)
-
-def binary_reminder_to_float(division_result_binary:str) -> float:
-
-    print(division_result_binary)
-
-    first_bin = division_result_binary[:8]
-    second_bin = division_result_binary[10:]
-
-    print(first_bin)
-    print(second_bin)
-
-    first_bin = str(binary_to_decimal_direct_code(first_bin))
-    second_bin = str(binary_to_decimal_direct_code(second_bin.zfill(8)))
-
-    result_float = first_bin + '.' + second_bin
-
-    print(result_float)
-
-    return float(result_float)
+    binary_representation = format(int_representation, '032b')
+    return binary_representation
 
 
+def add_floats(x, y):
+    if x < 0 or y < 0:
+        raise ValueError("Error Sign Value.")
+
+    x_binary = float_to_binary(x)
+    y_binary = float_to_binary(y)
+
+    x_sign = int(x_binary[0], 2)
+    x_exponent = int(x_binary[1:9], 2)
+    x_mantissa = int(x_binary[9:], 2)
+
+    y_sign = int(y_binary[0], 2)
+    y_exponent = int(y_binary[1:9], 2)
+    y_mantissa = int(y_binary[9:], 2)
+
+    x_mantissa |= (1 << 23)
+    y_mantissa |= (1 << 23)
+
+    if x_exponent > y_exponent:
+        y_mantissa >>= (x_exponent - y_exponent)
+        exponent = x_exponent
+    else:
+        x_mantissa >>= (y_exponent - x_exponent)
+        exponent = y_exponent
+
+    result_mantissa = x_mantissa + y_mantissa
+
+    if result_mantissa & (1 << 24):
+        result_mantissa >>= 1
+        exponent += 1
+
+    result_mantissa &= ~(1 << 23)
+
+    if exponent >= 255:
+        raise OverflowError("Exponent Overflow.")
+
+    result_binary = f"{0:01b}{format(exponent, '08b')}{format(result_mantissa, '023b')}"
+    result_float = struct.unpack('!f', struct.pack('!I', int(result_binary, 2)))[0]
+
+    return result_float, result_binary
 
 
 
--- Test/test_Binary_Operations.py
+++ Test/test_Binary_Operations.py
@@ -1,93 +1,85 @@
 from unittest import TestCase
-import BinaryOperations
+import Binary_Operations
 import pytest
 
 class Test(TestCase):
     def test_decimal_to_binary_direct_code(self):
-        binary_direct = BinaryOperations.decimal_to_binary_direct_code(2)
+        binary_direct = Binary_Operations.decimal_to_binary_direct_code(2)
         assert binary_direct == "00000010"
 
     def test_decimal_to_binary_direct_code_zero(self):
-        binary_direct = BinaryOperations.decimal_to_binary_direct_code(0)
+        binary_direct = Binary_Operations.decimal_to_binary_direct_code(0)
         assert binary_direct == "00000000"
 
     def test_decimal_to_binary_direct_code_negative(self):
-        binary_direct = BinaryOperations.decimal_to_binary_direct_code(-2)
+        binary_direct = Binary_Operations.decimal_to_binary_direct_code(-2)
         assert binary_direct == "10000010"
 
     def test_decimal_to_binary_inverse_code(self):
-        binary_inverse = BinaryOperations.decimal_to_binary_inverse_code(2)
+        binary_inverse = Binary_Operations.decimal_to_binary_inverse_code(2)
         assert binary_inverse == "00000010"
 
     def test_decimal_to_binary_additional_code(self):
-        binary_additional = BinaryOperations.decimal_to_binary_additional_code(2)
+        binary_additional = Binary_Operations.decimal_to_binary_additional_code(2)
         assert binary_additional == "00000010"
 
     def test_decimal_to_binary_inverse_code_negative(self):
-        binary_inverse = BinaryOperations.decimal_to_binary_inverse_code(-2)
+        binary_inverse = Binary_Operations.decimal_to_binary_inverse_code(-2)
         assert binary_inverse == "11111101"
 
     def test_decimal_to_binary_additional_code_negative(self):
-        binary_additional = BinaryOperations.decimal_to_binary_additional_code(-2)
+        binary_additional = Binary_Operations.decimal_to_binary_additional_code(-2)
         assert binary_additional == "11111110"
 
     def test_additional_code_binary_sum(self):
-        binary_additional = BinaryOperations.additional_code_binary_sum(BinaryOperations.decimal_to_binary_additional_code(-2),
-                                                                    BinaryOperations.decimal_to_binary_additional_code(2))
+        binary_additional = Binary_Operations.additional_code_binary_sum(Binary_Operations.decimal_to_binary_additional_code(-2),
+                                                                    Binary_Operations.decimal_to_binary_additional_code(2))
         assert binary_additional == "00000000"
 
     def test_binary_to_decimal_direct_code_negative(self):
-        decimal = BinaryOperations.binary_to_decimal_direct_code('10000010')
+        decimal = Binary_Operations.binary_to_decimal_direct_code('10000010')
         assert decimal == -2
 
     def test_binary_to_decimal_direct_code(self):
-        decimal = BinaryOperations.binary_to_decimal_direct_code('00000010')
+        decimal = Binary_Operations.binary_to_decimal_direct_code('00000010')
         assert decimal == 2
 
     def test_binary_to_decimal_additional_code(self):
-        decimal = BinaryOperations.binary_to_decimal_additional_code(BinaryOperations.decimal_to_binary_additional_code(3))
+        decimal = Binary_Operations.binary_to_decimal_additional_code(Binary_Operations.decimal_to_binary_additional_code(3))
         assert decimal == 3
 
     def test_binary_to_decimal_additional_code_negative(self):
-        decimal = BinaryOperations.binary_to_decimal_additional_code(BinaryOperations.decimal_to_binary_additional_code(-3))
+        decimal = Binary_Operations.binary_to_decimal_additional_code(Binary_Operations.decimal_to_binary_additional_code(-3))
         assert decimal == -3
 
     def test_binary_multiply(self):
-        binary_direct = BinaryOperations.binary_multiply('00001100','00000011')
-        decimal = BinaryOperations.binary_to_decimal_direct_code(binary_direct)
+        binary_direct = Binary_Operations.binary_multiply('00001100','00000011')
+        decimal = Binary_Operations.binary_to_decimal_direct_code(binary_direct)
         assert decimal == 36
 
     def test_binary_multiply_negative(self):
-        binary_direct = BinaryOperations.binary_multiply('00001100','10000011')
-        decimal = BinaryOperations.binary_to_decimal_direct_code(binary_direct)
+        binary_direct = Binary_Operations.binary_multiply('00001100','10000011')
+        decimal = Binary_Operations.binary_to_decimal_direct_code(binary_direct)
         assert decimal == -36
 
     def test_binary_multiply_negatives(self):
-        binary_direct = BinaryOperations.binary_multiply('10001100','10000011')
-        decimal = BinaryOperations.binary_to_decimal_direct_code(binary_direct)
+        binary_direct = Binary_Operations.binary_multiply('10001100','10000011')
+        decimal = Binary_Operations.binary_to_decimal_direct_code(binary_direct)
         assert decimal == 36
 
     def test_binary_division(self):
-        binary_direct = BinaryOperations.binary_division('00001100', '00000011')
-        assert binary_direct == '00000100.00000'
-
-    def test_binary_division_zero(self):
-        binary_direct = BinaryOperations.binary_division('00001100', '00000000')
-        assert binary_direct == 'Zero Division Error'
+        binary_direct = Binary_Operations.binary_divide('00001100', '00000011')
+        assert binary_direct == '00.00111'
 
     def test_binary_division_zero_dividend(self):
-        binary_direct = BinaryOperations.binary_division('00000000', '00000010')
+        binary_direct = Binary_Operations.binary_divide('00000000', '00000010')
         assert binary_direct == '0'
 
     def test_sum_floats(self):
-        float_num = BinaryOperations.sum_floats(2.5, 3.75)
+        float_num, binary_num = Binary_Operations.add_floats(2.5, 3.75)
         assert float_num == 6.25
 
-    def test_binary_reminder_to_float(self):
-        float_num = BinaryOperations.binary_reminder_to_float('00011')
-        assert float_num == 3.0
-
     def test_binary_subtract(self):
-        binary_num = BinaryOperations.binary_subtract('00000101', '00000010')
+        binary_num = Binary_Operations.binary_subtract('00000101', '00000010')
         assert binary_num == '11'
 
